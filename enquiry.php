da5d9ad79a8ff5693ffbc9b962d656b8d18e32ee
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-03-23 17:16:25 +0530

    updated the pages

---
+++ enquiry.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$msg = $_POST['msg'];
+	
+	$to = 'rdssigns@rdssignsindia.com';
+	$subject = 'From Enquiry Form';
+	$message = "Name: ".$name."\n"."Phone: ".$phone."\n". "Message: "."\n\n".$msg;
+	$headers = "From: ".$email;
+	
+	if(mail($to, $subject, $message, $headers)){		
+		echo "<h1>Sent Succesfully! Thank You"." ".$name.", We will Contact you shortly!</h1>";
+		echo '<a href="product.html">Click here</a>';
+		}
+			else{
+				echo "Went wrong!";				
+			}	
+}
+
+?>
